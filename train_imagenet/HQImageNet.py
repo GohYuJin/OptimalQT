2e333231c66fb80086c3ed0cc0337008796acb27
GohYuJin <trieightz@gmail.com>
2023-07-20 09:39:01 +0000

    renamed target_transforms to target_transform to avoid error again

--- train_imagenet/HQImageNet.py
+++ train_imagenet/HQImageNet.py
@@ -6,7 +6,7 @@ from typing import Any, Callable, cast, Dict, List, Optional, Tuple, Union
 
 class CustomImageNet(torchvision.datasets.VisionDataset):
     def __init__(self, root, impath_csv, imagenet_class_index_json, transforms=None, target_transform=None):
-        super().__init__(root, transforms=transforms, target_transforms=target_transform)
+        super().__init__(root, transforms=transforms, target_transform=target_transform)
         self.im_paths = open(impath_csv, "r").read().strip().split("\n")
         id_classname_json = json.load(open(imagenet_class_index_json, "r"))
         self.label_mapping = {v[0]:int(k) for k, v in id_classname_json.items()}
